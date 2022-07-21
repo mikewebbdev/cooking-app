160e159bb7ef2c987abf8b31cb6713deb6460608
mikewebbdev <mike.webb@orlo.tech>
2022-07-21 16:00:27 +0100

    add logic to allow selected recipe details to be shown

--- src/app/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Recipe } from "../../shared/recipe/recipe.model";
 
 @Component({
@@ -7,13 +7,20 @@ import { Recipe } from "../../shared/recipe/recipe.model";
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  @Output() selectedRecipe = new EventEmitter<{selectedRecipe: Recipe}>()
+
   recipes: Recipe[] = [
-    new Recipe('Test', 'testing class', 'https://cdn.pixabay.com/photo/2016/02/05/15/34/pasta-1181189_960_720.jpg')
+    new Recipe('Test', 'testing class', 'https://cdn.pixabay.com/photo/2016/02/05/15/34/pasta-1181189_960_720.jpg'),
+    new Recipe('Test2', 'testing 2 class', 'https://cdn.pixabay.com/photo/2016/02/05/15/34/pasta-1181189_960_720.jpg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(target: Recipe) {
+    this.selectedRecipe.emit({selectedRecipe: target});
   }
 
 }
--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../shared/recipe/recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  selectedRecipe: Recipe[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(target: { selectedRecipe: Recipe }) {
+    this.selectedRecipe[0] = target.selectedRecipe;
   }
 
 }
